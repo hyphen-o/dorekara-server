74d286f530b1c4d9613d45b2cfb6d983a397a134
keigo-0314 <okamoto.keigo@g.wakayama-u.jp>
2023-04-09 10:55:05 +0900

    Songコントローラの作成，メソッドの作成

---
+++ app/Http/Controllers/SongController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SongController extends Controller
+{
+    public function getAll() {
+
+    }
+
+    public function getOne() {
+
+    }
+
+    public function create() {
+
+    }
+
+    public function edit() {
+
+    }
+
+    public function delete() {
+
+    }
+}
