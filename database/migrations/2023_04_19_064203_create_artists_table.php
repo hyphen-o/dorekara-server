20b07f741bd235eb7eeb45a1a0d428e0896be3c4
hyphen-o <otekapu@gmail.com>
2023-04-19 16:44:47 +0900

    アーティストAPIの修正

--- database/migrations/2023_04_19_064203_create_artists_table.php
+++ database/migrations/2023_04_19_064203_create_artists_table.php
@@ -14,11 +14,7 @@ return new class extends Migration
         Schema::create('artists', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id')
-                  ->references('id')
-                  ->on('users')
-                  ->onDelete('cascade');
+            $table->timestamps();
         });
     }
 
---
+++ database/migrations/2023_04_19_064344_create_users_artists_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users_artists', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')
+                  ->references('id')
+                  ->on('users')
+                  ->onDelete('cascade');
+            $table->unsignedBigInteger('artist_id');
+            $table->foreign('artist_id')
+                  ->references('id')
+                  ->on('artists')
+                  ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users_artists');
+    }
+};
