fa66290e384fbfaac9f863d5d7810b85f4c2fff0
hyphen-o <otekapu@gmail.com>
2023-05-10 13:34:22 +0900

    ユーザ画像アップロード機能の作成

--- config/filesystems.php
+++ config/filesystems.php
@@ -14,6 +14,7 @@ return [
     */
 
     'default' => env('FILESYSTEM_DISK', 'local'),
+    'cloud' => env('FILESYSTEM_CLOUD', 's3'),
 
     /*
     |--------------------------------------------------------------------------
@@ -50,10 +51,7 @@ return [
             'secret' => env('AWS_SECRET_ACCESS_KEY'),
             'region' => env('AWS_DEFAULT_REGION'),
             'bucket' => env('AWS_BUCKET'),
-            'url' => env('AWS_URL'),
-            'endpoint' => env('AWS_ENDPOINT'),
-            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
-            'throw' => false,
+            'url' => env('AWS_URL')
         ],
 
     ],
